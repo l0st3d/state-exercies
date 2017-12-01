1040dc926283056202a0e4f9c04c3749536b51a2
Ed Bowler <ed.bowler@gmail.com>
2017-12-01 16:41:15 +0000

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject state-exercises "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [org.clojure/core.async "0.3.465"]
+                 [com.datomic/datomic-free "0.9.5651"]
+                 [org.clojure/test.check "0.9.0"]])
---
+++ test/state_exercises/core_test.clj
@@ -0,0 +1,273 @@
+(ns state-exercises.core-test
+  (:require [clojure.test :refer [deftest testing is are] :as t]
+            [clojure.repl :refer [doc source]]
+            [clojure.string :as string]
+            [clojure.core.async :refer [>! <! <!! >!!] :as async]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.edn :as edn])
+  (:import [java.util Date]))
+
+(deftest atoms
+  (let [a (atom {})]
+    (testing "basic updates and deref"
+      (swap! a assoc :something 1)
+      (is (= {:something 1} (deref a)))
+      (is (= {:something 1} @a)))
+    (testing "updates"
+      (swap! a update :something inc)
+      (is (= {:something 2} @a)))))
+
+(deftest refs
+  (testing "transactions"
+    (let [account-1          (ref {:balance 100.0M})
+          account-2          (ref {:balance 700.0M})
+          amount-to-transfer 10.0M]
+      (dosync
+       (alter account-1 update :balance - amount-to-transfer)
+       (alter account-2 update :balance + amount-to-transfer))
+      (is (= 90.0M (:balance @account-1)))
+      (is (= 710.0M (:balance @account-2))))))
+
+(deftest agents
+  (testing "asynchronous messages"
+    (let [a (agent {:c 1})
+          incr (fn [{:keys [c] :as a}]
+                 (prn 'agent 'executed '>> c)
+                 (assoc a :c (inc c)))]
+      (dotimes [n 10]
+        (send a incr))
+      (prn 'messages 'sent)
+      (await-for 10000 a)
+      (is (= 11 (:c @a)))))
+  (testing "errors"
+    (let [a (agent {:c 1})
+          err (fn [a]
+                (prn 'ERROR)
+                (throw (ex-info "Error Processing" {:agent-state a :message "Error"})))]
+      (send a err)
+      (Thread/sleep 1000)
+      (is (= "Error" (some-> a agent-error ex-data :message)))
+      (is (thrown? Exception (send a err)))
+      (restart-agent a {:c 0})
+      (is (= @a {:c 0})))))
+
+(def ^:dynamic *dynamic-var* ::global-state)
+
+(deftest vars
+  (testing "localised changes"
+    (is (= ::global-state *dynamic-var*))
+    (is (= ::global-state (deref #'*dynamic-var*)))
+    (binding [*dynamic-var* ::local-state]
+      (is (= ::local-state *dynamic-var*)))
+    (testing "from a different thread"
+      (-> (fn []
+            (binding [*dynamic-var* ::thread-local-state]
+              (Thread/sleep 100)
+              (prn '> 'state 'on (Thread/currentThread) *dynamic-var*)
+              (Thread/sleep 1000)
+              (is (= ::thread-local-state *dynamic-var*))))
+          Thread. .start)
+      (prn '> 'state 'on (Thread/currentThread) *dynamic-var*))))
+
+(deftest validators
+  (testing "atom validator"
+    (let [a (atom {} :validator #(if-let [c (:c %)]
+                                   (integer? c)
+                                   true))]
+      (swap! a assoc :c 0)
+      (swap! a update :c inc)
+      (is (= {:c 1} @a))
+      (is (thrown? Exception (swap! a assoc :c :not-valid))))))
+
+(defn agent-logger [k]
+  (let [agent-logger     (agent nil)
+        log-writer (fn [k r old new]
+                     (println (second k) (Date.) new))]
+    (add-watch agent-logger [::log-watcher k] log-writer)
+    (fn [& messages]
+      (send agent-logger (constantly (string/join " " messages)))
+      nil)))
+
+(deftest watchers
+  (testing "agent watcher"
+    (let [log (agent-logger ::test-agent-logger)]
+      (is (nil? (log "message" "to" {:you "Rudy"}))))))
+
+(def global-data (atom :test))
+
+(deftest mocking
+  (testing "with-redefs"
+    (is (= :test @global-data))
+    ;; with-redefs will temporarily redefine the contents of a var
+    (with-redefs [global-data (atom nil)]
+      (swap! global-data (constantly "fish"))
+      (is (= "fish" @global-data)))
+    (is (= :test @global-data))))
+
+;; Core Async
+
+(deftest go-blocks
+  (testing "reading from a channel with a timeout"
+    (let [log     (agent-logger :async-test)
+          _       (log "starting go test")
+          channel (async/chan)
+          done-ch (async/go-loop []
+                    (log "... waiting for input")
+                    (let [timeout (async/timeout 1000)
+                          result  (async/alt! channel ([x] (log "read" x) x)
+                                              timeout (log "timed out"))]
+                      (if result
+                        (recur)
+                        :finished)))]
+      (async/go
+        (dotimes [n 5]
+          (log ">> about to write" n)
+          (>! channel n)
+          (log ">> wrote" n)))
+      ;; >! and <! must be used inside a go block but <!! and >!! can be used in a normal Thread
+      (is (= :finished (<!! done-ch))))))
+
+(defn put-to-channel-up-to-n [channel n log]
+  (async/go
+    (dotimes [n n]
+      (log ">> about to write" n)
+      (>! channel n)
+      (log ">> wrote" n))))
+
+(defn take-from-channel-with-timeout
+  ([channel]
+   (take-from-channel-with-timeout channel prn))
+  ([channel log]
+   (async/go
+     (let [timeout (async/timeout 1000)]
+       (async/alt! channel ([x] (log "read" x) x)
+                   timeout (log "timed out"))))))
+
+(deftest go-block-with-buffers
+  (testing "lossy buffers"
+    (testing "dropping-buffer"
+      (let [log          (agent-logger :async-test)
+            _            (log "starting lossy buffers test")
+            channel      (async/chan (async/dropping-buffer 1))
+            done-putting (put-to-channel-up-to-n channel 5 log)
+            _            (<!! done-putting)
+            done-ch      (take-from-channel-with-timeout channel log)]
+        (is (= 0 (<!! done-ch)))))
+
+    (testing "sliding-buffer"
+      (let [log          (agent-logger :async-test)
+            _            (log "starting lossy buffers test")
+            channel      (async/chan (async/sliding-buffer 1))
+            done-putting (put-to-channel-up-to-n channel 5 log)
+            _            (<!! done-putting)
+            done-ch      (take-from-channel-with-timeout channel log)]
+        (is (= 4 (<!! done-ch)))))))
+
+(deftest pipeline
+  (let [input-chan  (async/chan 100)
+        output-chan (async/chan 100)]
+
+    (comment
+      ;; TODO use async/pipeline to transform the input strings
+      ;; - split out the numbers
+      ;; - filter out the odd numbers
+      ;;
+      ;; so for example, an input of
+
+      ["5em6k" "" "v" "94lpy" "T" "rvh12112I0PTG"]
+
+      ;; would be transformed into
+
+      [6 94 12112 0]
+
+      ;; transducers look like the sequence operations that we have
+      ;; been using over the course, but are combined with `comp`
+      ;; rather than `->>`. So where you would write:
+
+      (->> (range 1000)
+           (map inc)
+           (filter even?)
+           (take 10)
+           (into []))
+
+      ;; you now write;
+
+      (into []
+            (comp (map inc)
+                  (filter even?)
+                  (take 10))
+            (range 1000))
+
+
+      ;; remember that this:
+
+      (->> (range 1000)
+           (map inc)
+           (filter even?)
+           (take 10)
+           (into []))
+
+      ;; is the same as writing this:
+
+      (into [] (take 10 (filter even? (map inc (range 1000)))))
+
+      ;; Which you can prove to yourself by running this at the repl:
+      
+      (macroexpand '(->> (range 1000) (map inc) (filter even?) (take 10) (into [])))
+
+      )
+
+    
+    
+    (doseq [s (gen/sample (s/gen string?) 100)]
+      (async/put! input-chan s))
+
+    (loop [c 0]
+      (let [timeout (async/timeout 100)
+            x       (async/alt!! output-chan ([x] x)
+                                 timeout     :timeout)]
+        (if (number? x)
+          (do (is (even? x))
+              (recur (inc c)))
+          (is (> c 0)))))))
+
+
+(comment
+  ;; start the repl - or use proto-repl
+  lein repl
+
+  ;; load the namespace
+  (require '[state-exercises.core-test])
+
+  ;; switch to that namespace
+  (in-ns 'state-exercises.core-test)
+
+  ;; run a function
+  (go-block-with-buffers)
+
+  ;; run all tests
+  (t/run-tests)
+
+  ;;;; Tasks
+  ;;
+  ;; - Read through the tests and try separate how each of the
+  ;;   reference types operate. There are synchronous, co-ordinated,
+  ;;   unco-ordinated, dynamyic and isolated changes going on. Try to
+  ;;   apply labels to the different reference types.
+  ;;
+  ;; - put the classes data structure from week 3 in an atom and write
+  ;;   functions to
+  ;;   - store add a new class
+  ;;   - find all classes in a room
+  ;;   - find all classes on at a time
+  ;;   - find all classes for a given course
+  ;;
+  ;; - rewrite the logger to use channels instead of agents
+  ;;
+  ;; - use pipeline and a transducer to transform data and copy to
+  ;;   another chan, i.e. implement the pipeline test
+  ;;
+  
+
+  )
